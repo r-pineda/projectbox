ef4b8ba1813597b80d2da407fd089f10961bd52e
Rommelt Pineda <rommelt.pineda@gmail.com>
2017-09-07 12:39:28 +0200

    smoothscroll implemented

--- js/script.js
+++ js/script.js
@@ -7,38 +7,33 @@ window.sr = ScrollReveal({
 	easing: 'cubic-bezier(0, 0.2, 0, 1)'
 });
 
-
-/* Anchor (Scroll to) 
-$("#down").click(
-	function() {
-		scroll("#project");
-	});
-
-$("#linktodown").click(
-	function() {
-		scroll("#project");
-	});
-
-*/
-	
-/*
-$("#linktodoc").click(
-	function() {
-		scroll("#documents");
-	});
-
-$("#linktofunc").click(
-	function() {
-		scroll("#functions");
-	});
-
-$("#linktotop").click(
-	function() {
-		scroll("#top");
-	});
-*/
-
 sr.reveal('#fix');
 sr.reveal('#project-fade');
 sr.reveal('#team-fade');
 sr.reveal('#functions-fade');
+
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 700, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
