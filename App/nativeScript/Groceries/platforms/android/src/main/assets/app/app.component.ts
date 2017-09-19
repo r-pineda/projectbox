131b9eaabb647717f983f5a427cb3570acdc1155
michaelfruehwirth <michi888888889@gmail.com>
2017-09-19 23:03:09 +0200

    FRU new nativeScript test project

---
+++ App/nativeScript/Groceries/platforms/android/src/main/assets/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from "@angular/core";
+import { User } from "./shared/user/user";
+import { UserService } from "./shared/user/user.service";
+
+@Component({
+  selector: "my-app",
+  providers: [UserService],
+  templateUrl: "pages/login/login.html",
+  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
+})
+export class AppComponent {
+  user: User;
+  isLoggingIn = true;
+
+  constructor(private userService: UserService) {
+    this.user = new User();
+  }
+  submit() {
+    if (this.isLoggingIn) {
+      this.login();
+    } else {
+      this.signUp();
+    }
+  }
+  login() {
+    // TODO: Define
+  }
+  signUp() {
+    this.userService.register(this.user);
+  }
+  toggleDisplay() {
+    this.isLoggingIn = !this.isLoggingIn;
+  }
+}
