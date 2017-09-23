e99c0e05693c0d0841d80be90203df9957e7eb81
Rommelt Pineda <rommelt.pineda@gmail.com>
2017-09-23 19:58:38 +0200

    removed status bar in login screen

--- App/nativeScript/projectbox/platforms/android/src/main/assets/app/pages/login/login.component.ts
+++ App/nativeScript/projectbox/platforms/android/src/main/assets/app/pages/login/login.component.ts
@@ -3,6 +3,7 @@ import { User } from "../../shared/user/user";
 import { UserService } from "../../shared/user/user.service";
 import { Router} from "@angular/router";
 import {RouterExtensions} from "nativescript-angular/router";
+import {Page} from "ui/page";
 
 @Component({
   selector: "my-app",
@@ -14,10 +15,11 @@ export class LoginComponent {
   user: User;
   isLoggingIn = true;
 
-  constructor(private router: Router, private routerExtensions: RouterExtensions, private userService: UserService) {
+  constructor(private router: Router, private routerExtensions: RouterExtensions, private userService: UserService, private page: Page) {
     this.user = new User();
     this.user.email = "manuel.gafoz@htl.rennweg.at";
     this.user.password = "manuel1999";
+    page.actionBarHidden = true;
   }
   
   submit() {
