129b2cf8c2d28526702ca849bb8cedd29cb541e5
Michael Frühwirth <michael.fruehwirth99@gmx.at>
2017-09-27 12:22:59 +0200

    Revert "GAF: added iOS Building"
    This reverts commit 0770aa4536f8cc13d4325816593582e1411a3c12.

---
+++ App/nativeScript/projectbox/platforms/android/build/intermediates/assets/F0/debug/app/pages/list/list.component.ts
@@ -0,0 +1,48 @@
+import { Component } from "@angular/core";
+import { UserService } from "../../shared/user/user.service";
+import { Router} from "@angular/router";
+import {RouterExtensions} from "nativescript-angular/router";
+import {
+  GestureEventData,
+  GestureTypes,
+  PanGestureEventData,
+  PinchGestureEventData,
+  RotationGestureEventData,
+  SwipeGestureEventData,
+  TouchGestureEventData} from "ui/gestures";
+
+
+
+@Component({
+  selector: "list",
+  templateUrl: "pages/list/list.html",
+  providers: [UserService],
+  styleUrls: ["pages/list/list-common.css", "pages/list/list.css"]
+})
+export class ListComponent {
+
+  meetings :String;
+
+  constructor(private router: Router, private routerExtensions: RouterExtensions, private userService: UserService) {
+    
+    this.meetings = "Lade..."
+
+    this.userService.meetings().subscribe(
+      (data) => this.meetings = JSON.stringify(data),
+      (error) => alert("Unfortunately we could not find any meetings.")
+    );
+  }
+
+  onSwipe(args: SwipeGestureEventData) {
+    if (args.direction = 1) {
+    
+      this.routerExtensions.navigate(["/animation"], {
+          transition: {
+              name: "slide",
+              curve: "easeOut"
+          }
+      });
+    }
+  }
+
+}
