eecfb8aae342a0b8b14ea3099566597252b3dc0b
Manuel Gafoz <manuel.gafoz@gmail.com>
2017-09-28 15:51:31 +0200

    GAF: added 3D touch for iOS

--- App/nativeScript/projectbox/app/app.module.ts
+++ App/nativeScript/projectbox/app/app.module.ts
@@ -1,4 +1,7 @@
 import { NgModule } from "@angular/core";
+import { NgZone } from "@angular/core";
+import { isIOS } from "tns-core-modules/platform";
+import { RouterExtensions } from "nativescript-angular";
 import { NativeScriptFormsModule } from "nativescript-angular/forms";
 import { NativeScriptHttpModule } from "nativescript-angular/http";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
@@ -21,4 +24,38 @@ import { routes, navigatableComponents } from "./app.routing";
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+    constructor(private routerExtensions: RouterExtensions,
+              private zone: NgZone) {
+
+    new AppShortcuts().setQuickActionCallback(shortcutItem => {
+  console.log(`The app was launched by shortcut type '${shortcutItem.type}'`);
+
+      // this is where you handle any specific case for the shortcut, based on its type
+      if (shortcutItem.type === "dashboard") {
+        //this.deeplink("/page1");
+        console.log("dashboard");
+      } else if (shortcutItem.type === "task") {
+        //this.deeplink("/page2");
+        console.log("task");
+      } else if (shortcutItem.type === "meeting") {
+        //this.deeplink("/page2");
+        console.log("meeting");
+      } else if (shortcutItem.type === "ticket") {
+        //this.deeplink("/page2");
+        console.log("ticket");
+      } else if (shortcutItem.type === "seeting") {
+        //this.deeplink("/page2");
+        console.log("seeting");
+      }
+    });
+  }
+
+  private deeplink(to: string): void {
+    this.zone.run(() => {
+      this.routerExtensions.navigate([to], {
+        animated: false
+      });
+    });
+  }
+}
